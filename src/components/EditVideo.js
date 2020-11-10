0b7b4aa13ca57e91554ad812b148511a512f080f
manci <javiercaliescali@gmail.com>
2020-11-10 13:00:58 -0500

    edit video -error boundary

--- src/components/EditVideo.js
+++ src/components/EditVideo.js
@@ -4,22 +4,22 @@ import { NavLink } from 'react-router-dom';
 import { axios } from 'axios';
 
 const EditVideo = props => {
-  const { location: { edit: { video} } } = props;
-  const [title, setTitle] = useState(video.title);
-  const [description, setDescription] = useState(video.description);
-  const [url, setUrl] = useState(video.url);
-  const [year, setYear] = useState(video.year);
-  const [author, setAuthor] = useState(video.author);
+ // const { location: { edit: { video} } } = props;
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [url, setUrl] = useState('');
+  const [year, setYear] = useState('');
+  const [author, setAuthor] = useState('');
 
   const { match } = props;
   const { params } = match;
   const { id } = params;
  
 
-  const submitHandler = (id, title, description, url, year, author) => async () => {
-    console.log(title)
-   /*  try {
-      const { data } = await axios.put(`https://videosapi-gon.herokuapp.com/api/v1/videos/${id}`,
+  const submitHandler = (id, title, description, url, year, author) => {
+    // console.log(id, title, description, url, year, author)
+    try {
+      axios.put(`https://videosapi-gon.herokuapp.com/api/v1/videos/${id}`,
         {
           title,
           description,
@@ -28,11 +28,12 @@ const EditVideo = props => {
           author,         
         },
       );
-      console.log(data)
+      window.location.reload(false);
+      //console.log(data)
       props.history.push("/");
     } catch (error){ 
       console.log(error);
-    } */
+    }
   }
 
 
@@ -42,23 +43,23 @@ const EditVideo = props => {
         <form onSubmit={submitHandler(id,title, description, url, author, year)}>
           <label>
             Title:
-            <input type="text" name="title" placeholder={video.title} onChange={e => setTitle(e.target.value)} />
+            <input type="text" name="title" placeholder="title" onChange={e => setTitle(e.target.value)} />
           </label>
           <label>
             Description:
-            <input type="text" name="description" placeholder={video.url} onChange={e => setDescription(e.target.value)} />
+            <input type="text" name="description" placeholder="description" onChange={e => setDescription(e.target.value)} />
           </label>
           <label>
             Url:
-            <input type="text" name="url" placeholder={video.url} onChange={e => setUrl(e.target.value)} />
+            <input type="text" name="url" placeholder="url" onChange={e => setUrl(e.target.value)} />
           </label>
           <label>
             Author:
-            <input type="text" name="author" placeholder={video.author} onChange={e => setAuthor(e.target.value)} />
+            <input type="text" name="author" placeholder="author" onChange={e => setAuthor(e.target.value)} />
           </label>
           <label>
             Year:
-            <input type="date" name="year" onChange={e => setYear(e.target.value)} />
+            <input type="date" name="year" placeholder="year" onChange={e => setYear(e.target.value)} />
           </label>
           <input type="submit" value="Submit" />          
         </form>
@@ -72,6 +73,7 @@ EditVideo.propTypes = {
       id: PropTypes.string.isRequired,
     }),
   }).isRequired,
+ // video: PropTypes.objectOf(PropTypes.any),
 };
 
 export default EditVideo;
